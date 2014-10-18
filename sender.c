395485168724cacee7f6b2b678330b06cb4c3845
Jacquet Charles <charles.jacquet@student.uclouvain.be>
2014-10-19 00:52:04 +0200

    juste parce que j'ai un peu avancer

--- sender.c
+++ sender.c
@@ -2,20 +2,54 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <stdint.h>
+#include <zlib.h>
+#include <string.h>
 
 
 //création d'un structure de donnée pour envoyer les messages:
-typedef msgUDP struct msgUDP{
+typedef struct msgUDP{
 	uint8_t Type : 3;
 	uint8_t Window : 5;
 	uint8_t Seq_num;
 	uint16_t Length;
-	//je ne sais pas comment implémenter le payload de 512bytes dans la structur
-	//uLong crc32;
+	char payload[512];//je ne sais pas comment implémenter le payload de 512bytes dans la structure
+	int crc32; //j'ai pris un int parce qu'il a la bonne dimension, c'est a dire 4 bits ... mais normalement uLong crc32
+}__attribute__((packed)) msgUDP;
 
-} __attribute__((packed));
+typedef struct param{
+	char *filename;
+	char *hostname;
+	int port;
+	int sber;
+	int splr;
+	int delay;
+}param;
 
 
 int main(int argc, char *argv[]){
-	printf("Bonjour tu vas bien .. enfin voici un turk sympa ;) : %d " , sizeof(unsigned int));
+	//gestion des paramètres:
+	int i = 1;
+	char *filename;
+	int sber=0,splr=0, delay=0;
+	while(i<argc){
+		if(strcmp(argv[i], "--file")==0){
+			filename = argv[i+1];
+		}
+		if(strcmp(argv[i], "--sber")==0){
+			sber = atoi(argv[i+1]); // si il y a une erreur sber est mis à 0 ....
+		}
+		if(strcmp(argv[i], "--splr")==0){
+			splr = atoi(argv[i+1]); // mis à 0 en cas d'erreur
+		}
+		if(strcmp(argv[i], "--delay")==0){
+			delay = atoi(argv[i+1]); // mis à 0 en cas d'erreur
+		}
+
+		i++;
+	}
+	printf("filename %s,\nsber %d,\nsplr %d,\ndelay %d.\n", filename, sber, splr, delay);
 }
+
+
+
+
