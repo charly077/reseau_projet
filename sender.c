05ea700126b541c962e6969f9b17886d00ae568a
Jacquet Charles <charles.jacquet@student.uclouvain.be>
2014-10-19 11:39:20 +0200

    sender peut se connecter, il faut encore implémenter tous ce qui est de l'envoi

--- sender.c
+++ sender.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <stdint.h>
 #include <zlib.h>
 #include <string.h>
+
 
 
 //création d'un structure de donnée pour envoyer les messages:
@@ -16,24 +19,25 @@ typedef struct msgUDP{
 	int crc32; //j'ai pris un int parce qu'il a la bonne dimension, c'est a dire 4 bits ... mais normalement uLong crc32
 }__attribute__((packed)) msgUDP;
 
-typedef struct param{
-	char *filename;
-	char *hostname;
-	int port;
-	int sber;
-	int splr;
-	int delay;
-}param;
+
 
 
 int main(int argc, char *argv[]){
 	//gestion des paramètres:
 	int i = 1;
-	char *filename;
-	int sber=0,splr=0, delay=0;
+	char *filename; // Attention à bien vérifier le bool pour si on prend sur le stdin ou pas ...
+	int filenamegive=0; // le nom n'a pas été donné --> on prend sur le stdin
+	int sber=0,splr=0, delay=0; // pour les param
+	struct addrinfo *res, *addr; // pointeur vers le res qui sera l'info sur l'addresse du reveiver
+	struct addrinfo hints; // va nous permettre de dire qu'on veut du IPv6 et du UDP
+	int s; // pour gérer les erreur de getaddrinfo
+	int sock; // pour définir le socket
+
+
 	while(i<argc){
 		if(strcmp(argv[i], "--file")==0){
 			filename = argv[i+1];
+			filenamegive = 1;
 		}
 		if(strcmp(argv[i], "--sber")==0){
 			sber = atoi(argv[i+1]); // si il y a une erreur sber est mis à 0 ....
@@ -47,7 +51,43 @@ int main(int argc, char *argv[]){
 
 		i++;
 	}
-	printf("filename %s,\nsber %d,\nsplr %d,\ndelay %d.\n", filename, sber, splr, delay);
+	//printf("filename %s,\nsber %d,\nsplr %d,\ndelay %d.\n", filename, sber, splr, delay); // test pour vérification des arguments
+
+	//récuperation des information sur l'adresse du serveur ou receiver ..
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET6; // je veux juste de l'IPv6
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags=0;
+	hints.ai_protocol = 0 ; // peut importe le protocol ...
+	s = getaddrinfo(argv[argc-2], argv[-1], &hints, &res);
+	if (s != 0){
+		fprintf(stderr, "getaddrinfo: %s\n",gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
+	//res est liste de strucutures d'addresse 
+	for(addr = res; addr != NULL; addr = addr->ai_next){
+		sock =  socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+		if(sock == -1) continue; // si on arrive pas à créer un socket on continue
+		if(connect(sock, addr->ai_addr, addr->ai_addrlen) != 1) break; // si la connection à fonctionnée, on sort de la boucle
+
+		close(sock); // si on est pas sorti de la boucle il faut fermer le socket
+	}
+
+	if (addr = NULL){
+		fprintf(stderr, "Impossible de se connecter\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	//libérer les addrinfo:
+	freeaddrinfo(res); // on en a plus besoin après
+
+	
+
+
+
+
+	close(sock);
+
 }
 
 
