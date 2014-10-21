051fb0a88d7220e01c6fc61e3f362dfe9c6bfbc1
Stéphane Kimmel <Stef@StefMac.local>
2014-10-21 21:18:34 +0200

    Plus qu'une erreur mais pas complet... et questions intégrées

--- receiver.c
+++ receiver.c
@@ -3,7 +3,10 @@
 #include <sys/socket.h> // Pour la gestion des sockets
 #include <stdio.h>	// for fprintf 
 #include <string.h>	// for memcpy
-#include <zlib.h> // Pour le CRC
+#include <zlib.h>       // Pour le CRC
+#include <stdint.h>     // Pour les uint8_t, ...
+#include <netdb.h> 	   // à importer pour le getaddrinfo (surtout les structures avant)
+
 
 #define PACKET_SIZE 520
 #define PAYLOAD_SIZE 512
@@ -11,54 +14,15 @@
 #define PTYPE_DATA 1
 #define PTYPE_ACK 2
 
-// Doit etre remplacé par le sockaddr_in6 !!!!!!!!!
-struct in_addr {
-   in_addr_t	s_addr;
-};
+//   1) Problème lors de la compile des crc32 ...
+//   2) Mettre les packets d'IPv6 ?
+//   3) Pq pas mettre SOCK_RAW ?
+//   4) Comment faire pour les numéros de fenêtres / séquence ? 
 
-// Doit etre remplacé par le sockaddr_in6 !!!!!!!!!
-struct sockaddr_in {
-   uint8_t         sin_len;       /* longueur totale      */
-   sa_family_t     sin_family;    /* famille : AF_INET     */
-   in_port_t       sin_port;      /* le numéro de port    */
-   struct in_addr  sin_addr;      /* l'adresse internet   */
-   unsigned char   sin_zero[8];   /* un champ de 8 zéros  */
-};
+//
+//			!!! METTRE LES STRUCTURES EN IPv6 !!!
+//
 
-
-
-struct addrinfo {
-    int              ai_flags;
-    int              ai_family;
-    int              ai_socktype;
-    int              ai_protocol;
-    size_t           ai_addrlen;
-    struct sockaddr *ai_addr;
-    char            *ai_canonname;
-    struct addrinfo *ai_next;
-};
-
-
-struct in6_addr {
-    unsigned char   s6_addr[16];   // load with inet_pton()
-};
-
-
-struct sockaddr_in6 {
-    u_int16_t       sin6_family;   // address family, AF_INET6
-    u_int16_t       sin6_port;     // port number, Network Byte Order
-    u_int32_t       sin6_flowinfo; // IPv6 flow information
-    struct in6_addr sin6_addr;     // IPv6 address
-    u_int32_t       sin6_scope_id; // Scope ID
-};
-
-/*
-struct sockaddr {
-   unsigned char   sa_len;         // longueur totale 		
-   sa_family_t     sa_family;      // famille d'adresse 	
-   char            sa_data[14];    // valeur de l'adresse	
-};
-*/
 //création d'un structure de donnée pour envoyer les messages:
 typedef struct msgUDP{
 	uint8_t Type : 3;
@@ -69,10 +33,14 @@ typedef struct msgUDP{
 	int crc32;         //j'ai pris un int parce qu'il a la bonne dimension, c'est a dire 4 bits ... mais normalement uLong crc32 -> ouai, bizarre...
 }__attribute__((packed)) msgUDP;
 
+
+
+// MAIN --------------------------------------------------------
 int main(int argc, char *argv[])
 {
 	char *filename;
 	int port;
+	char *port_to_string;
 	char *hostname;
 
 	// On vérifie le nombre d'argument
@@ -88,14 +56,16 @@ int main(int argc, char *argv[])
 		filename = argv[2];
 		hostname = argv[3];
 		port = atoi(argv[4]);
+		port_to_string = argv[4];
 	}
 	else 
 	{
 		// prendre le filename sur le stdin
 		printf("Veuillez entrez le nom de l'hote ou l'adresse IPv6\n");
-		scanf("%s", &filename);			// Comment on fait pour l'extension ???
+		scanf("%s",filename);			// Comment on fait pour l'extension ???
 		hostname = argv[1];
 		port = atoi(argv[2]);
+		port_to_string = argv[2];
 	}
 
 	printf("Nom du programme : %s , nom du fichier : %s , nom de l'hote attendu : %s et numéro de port : %d\n", argv[0], filename, hostname, port);
@@ -109,17 +79,17 @@ int main(int argc, char *argv[])
 	struct addrinfo *adresse_sender;   /* pour pouvoir parcourir les éléments de la liste obtenue */
 	struct addrinfo template;			/* permet de définir le type de connexion voulue */
 
-    int socket;
+    //int socket;
 
  	// Obtention des infos sur le receveur
  	memset(&template, 0, sizeof(struct addrinfo)); // Met des 0 sur tout le buffer pour être sûr qu'il n'envoie pas de 1
 	template.ai_family = AF_INET6; // on veut de l'IPv6
 	template.ai_socktype =  SOCK_RAW; //SOCK_DGRAM;
 	template.ai_flags=0;
-	template.ai_protocol = IPPROTO_UDP; // 0; // peut importe le protocol
+	template.ai_protocol = 0; // IPPROTO_UDP; // 0; // peut importe le protocol
 
 	int s; // Variable d'erreur
-	if ((s = getaddrinfo(hostname, port, &template, &liste_obtenue)) != 0) // Nous permet d'avoir une liste de structure addrinfo qui concernent le client
+	if ((s = getaddrinfo(hostname, port_to_string, &template, &liste_obtenue)) != 0) // Nous permet d'avoir une liste de structure addrinfo qui concernent le client
 	{
 		fprintf(stderr, "getaddrinfo: %s\n",gai_strerror(s));
 		exit(EXIT_FAILURE);
@@ -183,7 +153,7 @@ int main(int argc, char *argv[])
         memcpy(calculerCRC_buf, packet_buf, strlen(packet_buf) - 4); 				// Je crée un buffer surlequel je vais pouvoir calculer le CRC
    		crc = crc32(crc, calculerCRC_buf, strlen(calculerCRC_buf));
 		
-		if (crc != msgUDP->crc32 || packet_struct->Type != PTYPE_DATA)
+		if (crc != packet_struct->crc32 || packet_struct->Type != PTYPE_DATA)
 		{
 			// discard packet
 			printf("Les 2 CRC ne correspondent pas, le packet doit être discardé \n");
@@ -196,19 +166,20 @@ int main(int argc, char *argv[])
 	        {
 	        	// Le numéro de packet est ok, je peux le sauver dans une des fenêtres !
 	        	// Mettre dans un buffer temporaire qui est dans une fenêtre
-		        payload_buf = packet_struct->payload;
+			strcpy(payload_buf, packet_struct->payload);        
+			//payload_buf = packet_struct->payload;
 
 
-		        // Mettre à jour le lastack
+		    // Mettre à jour le lastack
 
 
 
-		        // Ecriture dans le fichier	
-		        fwrite(payload_buf, 1, sizeof(payload_buf), fichier);	
-				printf("Nouveau message reçu et sauvé : %s\n", payload_buf);
+		    // Ecriture dans le fichier	
+		    fwrite(payload_buf, 1, sizeof(payload_buf), fichier);	
+			printf("Nouveau message reçu et sauvé : %s\n", payload_buf);
 
 
-				// Envoyer ACK
+			// Envoyer ACK
 
 	        }
 	        
@@ -218,3 +189,56 @@ int main(int argc, char *argv[])
 
     fclose(fichier);
 }
+
+
+
+/*
+// Doit etre remplacé par le sockaddr_in6 !!!!!!!!!
+struct in_addr {
+   in_addr_t	s_addr;
+};
+
+// Doit etre remplacé par le sockaddr_in6 !!!!!!!!!
+struct sockaddr_in {
+   uint8_t         sin_len;       // longueur totale     
+   sa_family_t     sin_family;    // famille : AF_INET    
+   in_port_t       sin_port;      // le numéro de port    
+   struct in_addr  sin_addr;      // l'adresse internet   
+   unsigned char   sin_zero[8];   // un champ de 8 zéros  
+};
+
+
+
+struct addrinfo {
+    int              ai_flags;
+    int              ai_family;
+    int              ai_socktype;
+    int              ai_protocol;
+    size_t           ai_addrlen;
+    struct sockaddr *ai_addr;
+    char            *ai_canonname;
+    struct addrinfo *ai_next;
+};
+
+
+struct in6_addr {
+    unsigned char   s6_addr[16];   // load with inet_pton()
+};
+
+
+struct sockaddr_in6 {
+    u_int16_t       sin6_family;   // address family, AF_INET6
+    u_int16_t       sin6_port;     // port number, Network Byte Order
+    u_int32_t       sin6_flowinfo; // IPv6 flow information
+    struct in6_addr sin6_addr;     // IPv6 address
+    u_int32_t       sin6_scope_id; // Scope ID
+};
+
+*/
+/*
+struct sockaddr {
+   unsigned char   sa_len;         // longueur totale 		
+   sa_family_t     sa_family;      // famille d'adresse 	
+   char            sa_data[14];    // valeur de l'adresse	
+};
+*/
