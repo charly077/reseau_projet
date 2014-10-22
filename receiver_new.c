7525d49f7a92beebe2a2e80e4c305de1970ac624
Stéphane Kimmel <Stef@StefMac.local>
2014-10-22 22:43:01 +0200

    Alpha_v1 mise à jour, buffer et écriture dans fichier améliorer

--- receiver_new.c
+++ receiver_new.c
@@ -26,7 +26,8 @@ int main(int argc, char *argv[])
 	char *port_to_string;
 	char *hostname;
 
-    if (argc != 3 && argc != 5) {
+    if (argc != 3 && argc != 5) 
+    {
         fprintf(stderr, "Usage: %s --file filename hostname port\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -50,7 +51,6 @@ int main(int argc, char *argv[])
 
 	printf("Nom du programme : %s , nom du fichier : %s , nom de l'hote attendu : %s et numéro de port : %d\n", argv[0], filename, hostname, port);
 
-
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_INET6;    /* Allow IPv4 or IPv6 */
     hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
 
 
     int s;
-    s = getaddrinfo(hostname, port_to_string, &hints, &result);
+    s = getaddrinfo(hostname, port_to_string, &hints, &result); //port_to_string
     if (s != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
         exit(EXIT_FAILURE);
@@ -67,8 +67,7 @@ int main(int argc, char *argv[])
 
     int sockett;
     for (rp = result; rp != NULL; rp = rp->ai_next) {
-        sockett = socket(rp->ai_family, rp->ai_socktype,
-                rp->ai_protocol);
+        sockett = socket(rp->ai_family, rp->ai_socktype,rp->ai_protocol);
         if (sockett == -1)
             continue;
 
@@ -91,9 +90,12 @@ int main(int argc, char *argv[])
     // ------------------------------------------------------------------
 	// Programme proprement dit :::
     
-    int length_receiv = PACKET_SIZE;
+    uint16_t length_receiv = PACKET_SIZE;
+    int nbre_bits_recu;
     char packet_buf[PACKET_SIZE]; 			// buffer de réception du packet
-    char payload_buf[PACKET_SIZE]; // PAYLOAD_SIZE    		// buffer de réception du payload
+    char payload_buf[PAYLOAD_SIZE]; // PAYLOAD_SIZE    		// buffer de réception du payload
+
+    struct msgUDP *packet_struct= NULL;
 
     char buffer_tot[255][PACKET_SIZE];
     int minimum = 0;    					// Minimum là ou on peut écrire
@@ -108,44 +110,70 @@ int main(int argc, char *argv[])
     	exit(1);
 	}
 
-	int j = 0;
-    while(length_receiv > 5) // définir un cran d'arrêt
+	int j = 0; 
+	int h = 0;
+    while(length_receiv == PACKET_SIZE && h < 3) // définir un cran d'arrêt
     {
+    	printf("On est dans la boucle !\n");
+    	h++;
     	// On reçoit un packet
-        length_receiv = recvfrom(sockett, packet_buf, PACKET_SIZE, 0, rp->ai_addr, &(rp->ai_addrlen)); //(struct sockaddr *) &addr_sender, &addrlen); // !!!! ON A LES INFOS DU SENDER QUI SE METTE DANS addr_sender
-        if (length_receiv == -1) {
-            printf("Problème, le message reçu est trop court...\n" );               // Ignore failed request 
+        nbre_bits_recu = recvfrom(sockett, packet_buf, PACKET_SIZE, 0, rp->ai_addr, &(rp->ai_addrlen)); // !!!! ON A LES INFOS DU SENDER QUI SE METTE DANS addr_sender
+        if (nbre_bits_recu == -1) {
+            printf("Problème, le message reçu n'est pas valide...\n" );               // Ignore failed request 
         }
 
         // On a reçu un buffer qui est en fait un packet. A la base, c'était une structure donc on la caste dans un structure.
-        // !!!!! PAS OPTIMISE, declarer le struct a l'exterieur de la boucle
-        //struct msgUDP *packet_struct = (struct msgUDP *) packet_buf;
-        strcpy(payload_buf, packet_buf);
-        // Ecriture dans le fichier	
-        //fprintf(fichier, "%s\n", payload_buf);
-        if(j >= minimum && j <= maximum)
+        packet_struct = (struct msgUDP *) packet_buf;
+
+        // Checker Type et CRC
+        /*
+        uLong crc = crc32(0L, Z_NULL, 0); 											// INT !!!!!!!!!
+        memcpy(calculerCRC_buf, packet_buf, strlen(packet_buf) - sizeof(uLong)); 				// Je crée un buffer surlequel je vais pouvoir calculer le CRC
+   		crc = crc32(crc, calculerCRC_buf, strlen(calculerCRC_buf));
+		*/
+
+		// Vérification de si c'est la fin des packets
+        if (packet_struct->length < PAYLOAD_SIZE)
         {
+        	printf("C'est l'envoi qui marque la fin de la connexion car < 512\n");
+        	break;
+        }
+		
+        // Copie de tous les éléments requis depuis la structure
+        strcpy(payload_buf, packet_struct->payload);
+        j = packet_struct->seq_num;
+        
+        if(j >= minimum && j <= maximum && packet_struct->type == PTYPE_DATA)
+        {
+        	// Copie dans le buffer
         	strcpy(buffer_tot[j], payload_buf);
+
+        	// Si c'est le plus petit élément, alors on déplace la fenêtre et on l'écrit dans le fichier.
         	if (j == minimum)
         	{
+        		// Ecriture dans le fichier	
+        		fprintf(fichier, "%s\n", buffer_tot[minimum]);
         		lastack++;
         		minimum++;
         		maximum++;
         	}
-        	j++; 																	// A supprimer
+        	j++;																// A supprimer
         }
         else
         {
         	printf("packet discardé car numéro hors de la fenêtre\n");
         }
 		
-		printf("Nouveau message reçu de longueur: %d\n", length_receiv);
+
+        // ENVOYER ACK
+
+		printf("Nouveau message reçu : type %d, window number : %d, sequence number : %d, length : %d et le CRC est %s \n", 
+			packet_struct->type, packet_struct->window, packet_struct->seq_num, packet_struct->length, packet_struct->crc32);
+		printf("Contenu : %s\n", packet_struct->payload_buf);
+
+
 
     }
-
-    fprintf(fichier, "%s\n", buffer_tot[0]);
-    fprintf(fichier, "%s\n", buffer_tot[1]);
-    fprintf(fichier, "%s\n", buffer_tot[2]);
 
 
     fclose(fichier);
